7854d8c2fab870373e16cd5798aee936b1f50aed
prii123 <printsvallejos@gmail.com>
2026-04-30 08:27:18 -0500

    .

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,8 +4,17 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  // log: ['query'],
-})
+const prismaClientSingleton = () => {
+  const url = process.env.DATABASE_URL_POOLED || process.env.DATABASE_URL
+
+  return new PrismaClient({
+    log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+    datasources: {
+      db: { url },
+    },
+  })
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
